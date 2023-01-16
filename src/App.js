005de5e0987ecde02fb99c367d63985c9df47aba
vanishree226 <121443678+vanishree226@users.noreply.github.com>
2023-01-16 13:34:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import Headers from './Components/Headers';
-import Home from './Components/Home';
+import Headers from 'Components/Headers';
+import Home from 'Components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
